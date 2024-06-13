26bc7617ecb0e476a88c4764964b4ce11a40785d
Tim Fry <tim@fusionpbx.com>
2024-06-13 16:11:18 -0300

    remove traits

---
+++ resources/interfaces/database_maintenance.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ *
+ * @author Tim Fry <tim@fusionpbx.com>
+ */
+interface database_maintenance {
+	public static function database_maintenance(settings $settings): void;
+}
---
+++ resources/interfaces/filesystem_maintenance.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ *
+ * @author Tim Fry <tim@fusionpbx.com>
+ */
+interface filesystem_maintenance {
+	public static function filesystem_maintenance(settings $settings): void;
+}
