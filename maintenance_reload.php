efc71d81b046e19d1a00d67f1a2efb8ae7db68b8
Tim Fry <tim@voipstratus.com>
2026-07-16 15:34:30 -0400

    Reload maintenance_service on settings reload The Maintenance Service must get a separate reload signal to update the settings because it is in a dedicated process unaffected by the php-fpm process.

--- maintenance_reload.php
+++ maintenance_reload.php
@@ -1,32 +1,40 @@
 <?php
+
 require_once dirname(__DIR__, 2) . "/resources/require.php";
 require_once "resources/check_auth.php";
 
+// Validate permissions
 if (!permission_exists('maintenance_edit')) {
         echo "access denied";
         exit;
 }
 
+// Add multi-lingual support
 $language = new text;
 $text = $language->get();
 
-//reload autoloader
+// Reload autoloader
 $autoload->update();
 
-//reload default settings
+// Reload default settings
 settings::clear_cache();
 
-//reset others
+// Reset others
 $classes_to_clear = array_filter($autoload->get_interface_list('clear_cache'), function ($class) { return $class !== 'settings'; });
 foreach ($classes_to_clear as $class_name) {
         $class_name::clear_cache();
 }
 
-//reset domains
+// Reset domains
 $domain = new domains();
 $domain->set();
 
+// Send the reload command to the maintenance service
+maintenance_service::send_reload();
+
+// Notify the user
 message::add($text['message-settings_reloaded']);
 
+// Redirect to the maintenance page and exit
 header("Location: maintenance.php");
 exit;
